b804aa4b3207e509450a495c7f68ed493bb37269
Richysgolds <richysgold@yahoo.com>
2022-10-19 10:59:29 -0700

    udated

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,37 +1,21 @@
 #include "main.h"
 
 /**
-
- *main - prints _putchar     
-
- * 
-
+ *main - prints _putchar
+ *
  *Description: prints _putchar
-
- *Return: 0 (Success)     
-
+ *Return: 0 (Success)
  */
+int main(void)
+{
+	char s[9] = "_putchar";
+	int i = 0;
 
-int main(void)                                                        
-
-{                                                                   
-
-                char s[9] = "_putchar";
-
-                int i = 0;
-
-                while (s[i] != '\0')
-
-                {                                               
-
-                _putchar(s[i]);
-
-                i++;
-
-                }                                               
-
+	while (s[i] != '\0')
+	{
+		_putchar(s[i]);
+		i++;
+	}
 _putchar('\n');
-
-        return (0);                                   
-
-}     
+return (0);
+}
