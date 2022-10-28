459789d219d55fab95e8e02b9200b557df76a7d1
Richysgolds <richysgold@yahoo.com>
2022-10-28 13:25:34 -0700

    Updated corrections

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ * Magic102 - Write a function Add
+ * one line to this code, so that the program
+ * prints a[2] = 98, followed by a new line.
+ * Remember:
+ * you are not allowed to use
+ * you are not allowed to modify p
+ * only one statement
+ * you are not allowed to code anything else than this line of code
+ */
+
+int main(void)
+{
+	int n;
+	int a[5];
+	int *pi;
+
+	a[2] = 1024;
+	p = &n;
+p[5] = 98;
+/* ...so that this prints 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+}
